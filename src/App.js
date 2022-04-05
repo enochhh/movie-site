fe244d7534ca0013f1d978cf1b5854c63a639590
enoch harnanto <enochharnantoo@gmail.com>
2022-04-04 20:19:46 -0700

    Add useState hook to fetch all search results and create movie cards for results

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import MovieCard from './components/MovieCard';
 
 // import './App.css';
@@ -17,12 +17,13 @@ const movie1 = {
 }
 
 const App = () => {
- 
+    const [movies, setMovies] = useState([]);
+
     const searchMovies = async(title) => {
         const response = await fetch(`${API_URL}&s=${title}`);
         const data = await response.json();
         
-        console.log(data.Search);
+        setMovies(data.Search);
     }
 
     useEffect(() => {
@@ -46,10 +47,21 @@ const App = () => {
                 >
                 </img>
             </div>
+            
+            {
+                movies?.length > 0 ? (
+                    <div className='container'>
+                        {movies.map((movie) => (
+                            <MovieCard prop={movie} />
+                        ))}
+                    </div>
+                ) : (
+                    <div className='empty'>
+                        <h2>No movies found</h2>
+                    </div>
+                )
+            }
 
-            <div className='container'>
-                <MovieCard prop={movie1}/>
-            </div>
         </div>
     );
 };
